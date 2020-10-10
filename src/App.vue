939c928fde246b145e8033de0bde30b6d818e0f2
shi_yi <shi_yidashi@163.com>
2020-10-10 17:47:43 +0800

    编辑左侧栏

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="app-head">
+      <div class="app-head-inner">
+        <router-link :to="{ path: '/' }" class="head-logo">
+          <img src="./assets/logo.png" />
+        </router-link>
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </div>
+    <div class="app-foot">
+      <p>© 2020 dashi MIT</p>
+    </div>
   </div>
 </template>
 
@@ -13,11 +35,65 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #444;
+  font-size: 14px;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+.app-head {
+  background: #363636;
+  color: #b2b2b2;
+  height: 90px;
+  line-height: 90px;
+  width: 100%;
+}
+.app-head-inner {
+  width: 1200px;
+  margin: 0 auto;
+}
+.head-logo {
+  float: left;
+  padding-top: 20px;
+  height: 50px;
+}
+.head-logo img {
+  width: 50px;
+}
+.head-nav {
+  float: right;
+}
+.head-nav ul {
+  overflow: hidden;
+}
+.head-nav li {
+  cursor: pointer;
+  float: left;
+}
+.nav-pile {
+  padding: 0 10px;
+}
+.app-foot {
+  text-align: center;
+  height: 80px;
+  width: 100%;
+  line-height: 80px;
+  background: #e3e4e8;
+  clear: both;
+  margin-top: 30px;
+}
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+.hr {
+  height: 1px;
+  width: 100%;
+  background: #ddd;
 }
 </style>
---
+++ src/pages/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="index-wrap">
+    <div class="index-left">
+      <div class="index-left-block">
+        <h2>全部产品</h2>
+        <div v-for="(product, index) in productList" :key="index">
+          <h3>{{ product.title }}</h3>
+          <ul>
+            <li v-for="(item, index) in product.list" :key="index">
+              <a :href="item.url">{{ item.name }}</a>
+              <span v-if="item.hot" class="hot-tag">HOT</span>
+            </li>
+          </ul>
+          <div v-if="!product.last" class="hr"></div>
+        </div>
+      </div>
+      <div class="index-left-block lastest-news">
+        <h2>最新消息</h2>
+        <ul>
+          <li v-for="(item, index) in newsList" :key="index">
+            <a :href="item.url" class="new-item">{{ item.title }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      productList: {
+        pc: {
+          title: 'PC产品',
+          list: [
+            {
+              name: '数据统计',
+              url: 'http://starcraft.com'
+            },
+            {
+              name: '数据预测',
+              url: 'http://warcraft.com'
+            },
+            {
+              name: '流量分析',
+              url: 'http://overwatch.com',
+              hot: true
+            },
+            {
+              name: '广告发布',
+              url: 'http://hearstone.com'
+            }
+          ]
+        },
+        app: {
+          title: '手机应用类',
+          last: true,
+          list: [
+            {
+              name: '91助手',
+              url: 'http://weixin.com'
+            },
+            {
+              name: '产品助手',
+              url: 'http://twitter.com',
+              hot: true
+            },
+            {
+              name: '智能地图',
+              url: 'http://maps.com'
+            },
+            {
+              name: '团队语音',
+              url: 'http://phone.com'
+            }
+          ]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.index-wrap {
+  width: 1200px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+.index-left {
+  float: left;
+  width: 300px;
+  text-align: left;
+}
+.index-right {
+  float: left;
+  width: 900px;
+}
+.index-left-block {
+  margin: 15px;
+  background: #fff;
+  box-shadow: 0 0 1px #ddd;
+}
+.index-left-block .hr {
+  margin-bottom: 20px;
+}
+.index-left-block h2 {
+  background: #4fc08d;
+  color: #fff;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+}
+.index-left-block h3 {
+  padding: 0 15px 5px 15px;
+  font-weight: bold;
+  color: #222;
+}
+.index-left-block ul {
+  padding: 10px 15px;
+}
+.index-left-block li {
+  padding: 5px;
+}
+.hot-tag {
+  background: red;
+  color: #fff;
+}
+</style>
